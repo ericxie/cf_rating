a7bbedf91ed14ac0cf43c75149f2797ba47c32e2
ericxie <ericxieforever@gmail.com>
2019-07-18 11:14:08 +0800

    add the detail of last5,fix some debug

--- cf_rating.py
+++ cf_rating.py
@@ -8,32 +8,37 @@ import numpy as np
 import os
 import time
 
-def getStyleCell(handle,rating,flag=False):
-	if flag:
-		handle = '<a href="http://codeforces.com/profile/%s" target="_blank">%s</a>'%(handle,handle)
+
+def getStyleRating(rating,text):
 	if rating == 0:
-		ret = '<td class="U">%s</td>'%handle
+		ret = '<span class="U">%s</span>'%text
 	elif rating < 1200:
-		ret = '<td class="N">%s</td>'%handle
+		ret = '<span class="N">%s</span>'%text
 	elif rating < 1400:
-		ret = '<td class="P">%s</td>'%handle
+		ret = '<span class="P">%s</span>'%text
 	elif rating < 1600:
-		ret = '<td class="S">%s</td>'%handle
+		ret = '<span class="S">%s</span>'%text
 	elif rating < 1900:
-		ret = '<td class="E">%s</td>'%handle
+		ret = '<span class="E">%s</span>'%text
 	elif rating < 2100:
-		ret = '<td class="CM">%s</td>'%handle
+		ret = '<span class="CM">%s</span>'%text
 	elif rating < 2300:
-		ret = '<td class="M">%s</td>'%handle
+		ret = '<span class="M">%s</span>'%text
 	elif rating < 2400:
-		ret = '<td class="IM">%s</td>'%handle
+		ret = '<span class="IM">%s</span>'%text
 	elif rating < 2600:
-		ret = '<td class="GM">%s</td>'%handle
+		ret = '<span class="GM">%s</span>'%text
 	elif rating < 3000:
-		ret = '<td class="IGM">%s</td>'%handle
+		ret = '<span class="IGM">%s</span>'%text
 	else:
-		ret = '<td class="N"><span class="LGM1">%s</span>%s</td>'%(handle[0],handle[1:])
+		ret = '<span class="LGM1">%s</span><span class="LGM">%s</span>'%(text[0],text[1:])
 	return ret
+
+def getStyleText(handle,rating,flag=False):
+	text = getStyleRating(rating,handle)
+	if flag:
+		text = '<a href="http://codeforces.com/profile/%s" target="_blank">%s</a>'%(handle,text)
+	return text
 
 class User(object):
 	def __init__(self,handle,name):
@@ -45,17 +50,26 @@ class User(object):
 		self.minR = 0
 		self.maxR = 0
 		self.last5 = 0
+		self.ratingChange = []
 		self.lastContestDate = 'no_contest'
-		
+
+	def ratingChangeToString(self):
+		text = ''
+		if self.cnt > 0: 
+			for x in self.ratingChange[:-1]:
+				text = text + getStyleText(str(x),x) + '&rarr;'
+			text = text + getStyleText(str(self.ratingChange[-1]),self.ratingChange[-1])
+		return text
+
 	def prt(self):
 		print("%s,%s,%s,%d,%d,%d,%d,%d,%s,%s"%(self.handle,self.name,self.status,self.cnt,self.cur,self.minR,self.maxR,self.last5,self.lastContestDate))
 	def toHtml(self):
-		html = getStyleCell(self.handle,self.cur,True)
+		html = '<td>%s</td>'%getStyleText(self.handle,self.cur,True)
 		html = html + '<td>%s</td><td>%s</td><td>%d</td>'%(self.name,self.status,self.cnt)
-		html = html + getStyleCell(str(self.cur),self.cur)
-		html = html + getStyleCell(str(self.minR),self.minR)
-		html = html + getStyleCell(str(self.maxR),self.maxR)
-		html = html + getStyleCell(str(self.last5),self.last5)
+		html = html + '<td>%s</td>'%getStyleText(str(self.cur),self.cur)
+		html = html + '<td>%s</td>'%getStyleText(str(self.minR),self.minR)
+		html = html + '<td>%s</td>'%getStyleText(str(self.maxR),self.maxR)
+		html = html + '<td>%s&ensp; / &ensp; %s</td>'%(self.ratingChangeToString(),getStyleText(str(self.last5),self.last5))
 		html = html + '<td>%s</td>'%(self.lastContestDate)
 		return html
 
@@ -101,8 +115,12 @@ def getUsersRating(users):
 				localtime = time.localtime(rating['result'][-1]['ratingUpdateTimeSeconds'])
 				dt = time.strftime('%Y-%m-%d',localtime)
 				user.lastContestDate = dt;
+				k = min(user.cnt,5)
+				user.ratingChange.append(rating['result'][-k]['oldRating'])
+				user.ratingChange.extend([x['newRating'] for x in rating['result'][-k:]])
 				if user.cnt >= 5:
 					user.last5 = int(round(np.mean(ratingList[-5:])))
+
 		else:
 			user.status = 'non-existent'
 
@@ -120,7 +138,7 @@ def saveHtml(users,filename):
 		<div>
 			<table id="rating">
 			<tbody>
-				<tr><th>排名</th><th>账号</th><th>姓名</th><th>状态</th><th>场次</th><th>当前</th><th>最低</th><th>最高</th><th>最近5场平均</th><th>最近比赛日期</th></tr>
+				<tr><th>排名</th><th>账号</th><th>姓名</th><th>状态</th><th>场次</th><th>当前</th><th>最低</th><th>最高</th><th>最近5场/平均</th><th>最近比赛日期</th></tr>
 	''';
 	fp.write(html_head)
 	rank = 0
